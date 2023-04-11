2ae69783a10672c9a94fe0338178d821b4864a08
showermax <sevenhauz@gmail.com>
2023-04-11 19:06:50 +0300

    lesson 7 checkbox

--- src/1104/AddItemForm.tsx
+++ src/1104/AddItemForm.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
@@ -24,24 +25,23 @@ export function AddItemForm(props: AddItemFormPropsType) {
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         setError(null);
-        if (e.charCode === 13) {
+        if (e.key === 'Enter') {
             addItem();
         }
     }
     const btnStyle ={
-        maxWidth: '30px',
-        maxHeight: '30px',
-        minWidth: '30px',
-        minHeight: '30px'
+        maxWidth: '38px',
+        maxHeight: '38px',
+        minWidth: '38px',
+        minHeight: '38px'
     }
     return <div>
-        <input value={title}
-               onChange={onChangeHandler}
-               onKeyPress={onKeyPressHandler}
-               className={error ? "error" : ""}
-        />
+
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" size='small' value={title}
+                   onChange={onChangeHandler}
+                   onKeyDown={onKeyPressHandler}/>
         {/*<button onClick={addItem}>+</button>*/}
-        <Button variant="contained" size="small" style={btnStyle}>+</Button>
+        <Button variant="contained" size="small" style={btnStyle} onClick={addItem}>+</Button>
         {error && <div className="error-message">{error}</div>}
     </div>
 }
